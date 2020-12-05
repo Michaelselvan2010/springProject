e09621d4685076de48f0ddd9dea8146109065868
DevopsMichael2020 <71185917+DevopsMichael2020@users.noreply.github.com>
2020-12-05 18:12:37 +0530

    Update SpringBootJdbcApplication.java

--- src/main/java/SpringBootJdbcApplication.java
+++ src/main/java/SpringBootJdbcApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootJdbcApplication {  
     public static void main(String[] args) {  
         SpringApplication.run(SpringBootJdbcApplication.class, args); 
+        system.out.println("error expecting...");
+      
         System.out.println("Inserted !!!!!!!!!!!");
     }  
 }  
